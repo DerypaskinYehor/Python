34f94f28c17cdb8fdf50b90143f7b71bdcd64ee7
DerypaskinYehor <egorderipaskin1021@gmail.com>
2025-12-18 12:22:40 +0200

    Update Завдання №3.py

--- практична робота №1/Завдання №3.py
+++ практична робота №1/Завдання №3.py
@@ -1,21 +1,9 @@
-def draw_number_pattern():
-    print("Числовий візерунок")
-    try:
-        user_input = input("Введіть ціле число: ")
-        N = int(user_input)
+N = int(input("Введіть число N: "))
 
-        if 1 < N < 9:
-            print(f"\nРезультат для цифри = {N}:")
-            for i in range(1, N + 1):
-                for j in range(N, i - 1, -1):
-                    print(j, end=" ")
-                
-                print()
-        else:
-            print("Число має бути більше 1 та менше 9.")
-            
-    except ValueError:
-        print("Будь ласка, введіть ціле число.")
-
-if __name__ == "__main__":
-    draw_number_pattern()
+if 1 < N < 9:
+    for i in range(N):
+        for j in range(N, i, -1):
+            print(j, end=" ")
+        print()
+else:
+    print("Число повинно задовольняти умову 1 < N < 9.")
